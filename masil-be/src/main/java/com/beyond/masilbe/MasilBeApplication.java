456e7b315e931d8601d8ffc1166341a1532b3b46
wjdvlf5456 <wjdvlf1407@gmail.com>
2026-01-08 22:45:19 +0900

    conf: init

---
+++ masil-be/src/main/java/com/beyond/masilbe/MasilBeApplication.java
@@ -0,0 +1,12 @@
+package com.beyond.masilbe;
+
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasilBeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MasilBeApplication.class, args);
+    }
+}
---
+++ masil-be/src/test/java/com/beyond/masilbe/MasilBeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.beyond.masilbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasilBeApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
