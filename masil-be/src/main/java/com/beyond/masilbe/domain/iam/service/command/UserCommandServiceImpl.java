1b0454da38c4f2dcbb17bbdcd0e01d9fc1b10a10
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2026-01-26 21:36:21 +0900

    로그인 기능 구현 (#10)
    feat: 로그인 구현

--- masil-be/src/main/java/com/beyond/masilbe/domain/iam/service/command/UserCommandServiceImpl.java
+++ masil-be/src/main/java/com/beyond/masilbe/domain/iam/service/command/UserCommandServiceImpl.java
@@ -4,7 +4,6 @@ import com.beyond.masilbe.domain.iam.dto.user.request.CreateUserRequestDto;
 import com.beyond.masilbe.domain.iam.dto.user.response.CreateUserResponseDto;
 import com.beyond.masilbe.domain.iam.entity.Users;
 import com.beyond.masilbe.domain.iam.support.user.UserWriter;
-import com.beyond.masilbe.security.PasswordGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,11 +22,9 @@ public class UserCommandServiceImpl implements UserCommandService {
     @Transactional
     public CreateUserResponseDto createUser(final CreateUserRequestDto request) {
 
-        // 임시 비밀번호 생성
-        final String tempPassword = PasswordGenerator.generate();
-        final String encrypted = passwordEncoder.encode(tempPassword);
+        final String encryptedPassword = passwordEncoder.encode(request.getPassword());
 
-        Users users = Users.create(request, encrypted);
+        Users users = Users.create(request, encryptedPassword);
         Users saved = userWriter.save(users);
 
         return CreateUserResponseDto.fromEntity(saved);
